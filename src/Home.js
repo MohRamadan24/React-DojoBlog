40b4aba105ee456688665441fd04256127dc7d72
MOH. RAMADAN <moh.ramadan8h@gmail.com>
2024-12-02 10:46:38 +0700

    duplicate from react first

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,18 @@
-
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-    useEffect(() => {
-        fetch('http://localhost:3004/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false);
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    }, []);
+    
 
     return (  
         <div className="home">
             { isPending && <div>Loading...</div>}
+            { error && <div>{ error }</div> }
             {blogs && <BlogList blogs={ blogs } title="All Blogs!"/>}
             {/* <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title="Mario's Blogs!" /> */}
         </div>
     );
 }
- 
 export default Home;
